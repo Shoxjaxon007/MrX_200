97252b8af4716185d2a80b29b49181e5ea3ef626
Shoxjaxon007 <shakarovshoxjaxon666@gmail.com>
2023-08-11 15:38:04 +0500

    fix

---
+++ mrx.js
@@ -0,0 +1,12 @@
+let a = prompt("Sonni kiriting")
+
+
+if ( a%3===0) {
+console.log("Fizz");
+}
+if (a%5===0) {
+    console.log("Buzz");
+}
+else if (a%5===0 && a%3===0) {
+    console.log("FizzBuzz");
+}
